75825d68a66645fc86958416b9518591047c8034
PrivateUserSource <projectferamp@gmail.com>
2024-08-14 10:03:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const starCount = 150;
     const stars = document.getElementById('stars');
-    const visitCountElement = document.getElementById('visitCount');
     const backgroundMusic = document.getElementById('backgroundMusic');
     const toggleMusicButton = document.getElementById('toggleMusic');
     const disclaimerText = document.getElementById('disclaimer');
@@ -71,7 +70,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const apiUrl = 'https://api.fera.dev';
 
-    visitCountElement.textContent = 'Loading Visits...';
     disclaimerText.textContent = 'Loading Disclaimer...';
 
     fetch(`${apiUrl}/disclaimer`)
@@ -91,48 +89,5 @@ document.addEventListener('DOMContentLoaded', () => {
     .catch(error => {
         console.error('Error fetching disclaimer:', error);
         disclaimerText.style.display = 'none';
-    });
-
-
-    fetch(`${apiUrl}/visit-count`)
-    .then(response => {
-        console.log('Response status:', response.status); // Log status
-        if (!response.ok) {
-            throw new Error(`Network response was not ok. Status: ${response.status}`);
-        }
-        return response.json();
-    })
-    .then(data => {
-        console.log('Response data:', data); // Log data to inspect it
-        visitCountElement.textContent = `Visits: ${data.visitCount}`;
-    })
-    .catch(error => {
-        console.error('Error fetching visit count:', error);
-        visitCountElement.textContent = 'Error retrieving visit count';
-    });
-
-   // Fetch IP address and increment visit count
-  fetch('https://api.ipify.org?format=json')
-    .then(response => response.json())
-    .then(data => {
-        let ipAddress = data.ip;
-        
-        // Function to extract IPv4 address from IPv6 if necessary
-        const extractIPv4FromIPv6 = (ip) => {
-            // Check if the address starts with '::ffff:'
-            if (ip.startsWith('::ffff:')) {
-                return ip.slice(7); // Remove the '::ffff:' prefix
-            }
-            return ip; // Return the IP as is if it's already IPv4
-        };
-        
-        // Extract the IPv4 address if needed
-        ipAddress = extractIPv4FromIPv6(ipAddress);
-
-        // Make the POST request with the cleaned IPv4 address
-        return fetch(`${apiUrl}/increment-visit/${ipAddress}`, { method: 'POST' });
-    })
-    .catch(error => {
-        console.error('Error incrementing visit count:', error);
     });
 });
